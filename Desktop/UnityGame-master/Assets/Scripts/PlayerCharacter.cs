1b13516d346eaba17d971f3fe764c33254d71670
Ltejeda <the01game@yahoo.com>
2018-10-19 23:06:16 -0700

    health ammo teleport

---
+++ Desktop/UnityGame-master/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+// Includes player health, ammo, death and teleport
+public class PlayerCharacter : MonoBehaviour {
+    public int _health;
+    public int _ammo;
+
+    public Slider healthBar;
+    public Image damageImage;
+    public Text ammoCount;
+
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    RayShooter playerShooting;
+    FPSInput playerMoving;
+    public bool isDamaged;
+    public bool isDead;
+    // Use this for initialization
+    void Start () {
+        _health = 100;
+<<<<<<< HEAD
+        _ammo = 30;
+=======
+        _ammo = 200;
+>>>>>>> 8d0716853c44eec68b39bbd9e9e795a840d24c96
+        playerMoving = GetComponent<FPSInput>();
+        playerShooting = GetComponentInChildren<RayShooter>();
+	}
+	public void Hurt(int damage)
+    {
+        isDamaged = true;
+        _health -= damage;
+        healthBar.value = _health;
+        Debug.Log("Player hit hp: " + _health);
+        if (_health <= 0  && !isDead)
+        {
+            Debug.Log("Player Dead!");
+            Dead();
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        ammoCount.text = _ammo.ToString();
+        if(isDamaged)
+        {
+            damageImage.color = flashColor;
+            isDamaged = false;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+    }
+    // Disable player shooting and moving when dead
+    void Dead()
+    {
+        if (_health <= 0)
+        {
+            isDead = true;
+            playerShooting.enabled = false;
+            playerMoving.enabled = false;
+        }
+    }
+    void OnControllerColliderHit(ControllerColliderHit col)
+    {
+        if (col.collider.gameObject.tag == "Hotspot")
+        {
+            //Randomize player location on collision with hotspot
+            float playerx = Random.Range(-20, 20);
+            float playerz = Random.Range(-20, 20);
+            Debug.Log("Teleport position: " + playerx + " " + playerz);
+            transform.position = new Vector3(playerx, 0, playerz);
+        }
+        // When player touches health pack increase health by 1 and delete health pack
+        if (col.collider.gameObject.tag == "Healthpack")
+        {
+            _health += 1;
+            Destroy(col.gameObject);
+            Debug.Log("Touched health pack hp is: " + _health);
+        }
+        // When player touches ammo pack increase ammo by 10 and delete ammo pack
+        if (col.collider.gameObject.tag == "Ammopack")
+        {
+            _ammo += 10;
+            Destroy(col.gameObject);
+            Debug.Log("Touched ammo pack: " + _ammo);
+        }
+    }
+}
---
+++ Desktop/UnityGame-master/Assets/Scripts/Runner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//For the bystander to just run away and destroy when hitting a wall
+public class Runner : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+    public Transform detectPlayer;
+    public float closeDistance = 8.0f;
+    private bool _alive;
+    GameObject player;
+
+    private void Start()
+    {
+        _alive = true;
+        player = GameObject.FindGameObjectWithTag("Player");
+        detectPlayer = player.transform;
+    }
+    // Use this for initialization
+    void Update()
+    {
+        if (_alive)
+        {
+            if(detectPlayer)
+            {
+                Vector3 offset = detectPlayer.position - transform.position;// Used to find if player is close
+                float sqrLen = offset.sqrMagnitude;
+                if (sqrLen < closeDistance * closeDistance)
+                {
+                    //float angle = Random.Range(-110, 110);
+                    //transform.Rotate(0, angle, 0);
+                    transform.Translate(0, 0, speed * Time.deltaTime);//move
+                }
+            }
+        }
+    }
+    void OnControllerColliderHit(ControllerColliderHit col)
+    {
+        if (col.collider.gameObject.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public void SetAlive(bool alive)
+    {
+        _alive = alive;
+    }
+}
