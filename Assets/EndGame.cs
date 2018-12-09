429f9e17eaa7604153948769ba253a7f826fb4cd
Joshua Guizarnotegui <el_capitrucho@yahoo.com>
2018-12-08 23:27:00 -0800

    added endscene

---
+++ Assets/EndGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour {
+    GameObject player;
+    PlayerCharacter playerHealth;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerCharacter>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (playerHealth._health == 0)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+	}
+}
--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -14,4 +14,8 @@ public class MainMenu : MonoBehaviour {
         Debug.Log("Quit!");
         Application.Quit();
     }
+    public void RestartGame ()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 }
